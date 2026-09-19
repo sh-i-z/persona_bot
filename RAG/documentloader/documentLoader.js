38b0c43c7e36248dc6272eb2c099938f642e6084
Shruti-369 <alewashruti@gmail.com>
2026-09-19 22:49:44 +0530

    dl.js

--- RAG/documentloader/documentLoader.js
+++ RAG/documentloader/documentLoader.js
@@ -2,14 +2,19 @@ import path from "path";
 
 export async function extractText(file) {
 
+    console.log("🔥 LOADER CALLED");
+
     const extension = path.extname(file.originalname).toLowerCase();
 
-    // Text based files
+    console.log("Extension:", extension);
+
     if (
         extension === ".txt" ||
         extension === ".md" ||
         extension === ".markdown"
     ) {
+        console.log("🔥 Reading text file");
+
         return file.buffer.toString("utf-8");
     }
 
